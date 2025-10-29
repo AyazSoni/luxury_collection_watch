d7f65b328fd60e0877541fed81d8868aa5668ee2
Ayaz Soni <ayaz.soni@winvesta.in>
2025-10-30 00:31:38 +0530

    ip

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -14,36 +14,42 @@ const Navbar = () => {
     const currentPath = location.pathname;
 
     return (
-        <div className='w-full fixed top-0 left-0 z-20'>
-            <div className='md:flex items-center justify-between bg-gray-100 py-4 md:px-10 px-7'>
+        <div className='w-full fixed top-0 left-0 z-30 bg-white shadow-md'>
+            <div className='md:flex items-center justify-between bg-gray-100 py-4 md:px-10 px-7 relative'>
                 {/* logo section */}
-                <div className='font-bold text-xl cursor-pointer flex items-center gap-1'>
-                    <span className="thick-font">Luxury Collection</span>
+                <div className='font-bold text-xl cursor-pointer flex items-center gap-1 z-40'>
+                    <Link to="/" className="thick-font text-gray-800 hover:text-customGreen transition-colors">
+                        Luxury Collection
+                    </Link>
                 </div>
-                {/* Menu icon */}
-                <div onClick={() => setOpen(!open)} className='absolute right-8 bottom-4 cursor-pointer md:hidden w-7 h-7'>
+                
+                {/* Menu Press icon - Mobile only */}
+                <div onClick={() => setOpen(!open)} className='absolute right-8 top-1/2 -translate-y-1/2 cursor-pointer md:hidden w-7 h-7 z-40'>
                     {
-                        open ? <XMarkIcon /> : <Bars3BottomRightIcon />
+                        open ? <XMarkIcon className="w-7 h-7" /> : <Bars3BottomRightIcon className="w-7 h-7" />
                     }
                 </div>
+                
                 {/* link items */}
-                <ul className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-gray-100 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
+                <ul className={`md:flex md:items-center md:pb-0 pb-4 absolute md:static bg-gray-100 md:z-auto z-30 left-0 w-full md:w-auto md:pl-0 pl-9 pt-4 md:pt-0 transition-all duration-500 ease-in ${open ? 'top-16 md:top-0' : 'top-[-490px] md:top-0'}`}>
                     {
                         Links.map((link) => {
                             const isActive = currentPath === link.link;
                             return (
-                                <li key={link.name} className='md:ml-8 md:my-0 my-6 font-semibold'>
-                                    <Link to={link.link} className={`text-gray-600 hover:text-gray-800 duration-500 thick-font ${isActive ? 'text-blue-500' : ''}`}>
+                                <li key={link.name} className='md:ml-8 md:my-0 my-4 font-semibold'>
+                                    <Link 
+                                        to={link.link} 
+                                        onClick={() => setOpen(false)}
+                                        className={`text-gray-600 hover:text-customGreen duration-300 thick-font text-base ${isActive ? 'text-customGreen font-bold' : ''}`}
+                                    >
                                         {link.name}
                                     </Link>
-                                    <hr />
+                                    <hr className="md:hidden mt-2 border-gray-300" />
                                 </li>
                             );
                         })
                     }
                 </ul>
-
-                {/* button */}
             </div>
         </div>
     );
